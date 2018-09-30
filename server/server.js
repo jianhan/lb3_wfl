3f23cf5d6a1ffed25be2ebe8c2d461e5473082b2
Jian Han <jianhan@mail.com>
2018-09-30 12:43:09 +1000

    [feat] Remove providers , updated server.js

--- server/server.js
+++ server/server.js
@@ -5,9 +5,13 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
-app.start = function() {
+app.get('/auth/account', function (req, res, next) {
+  console.log(req.user)
+});
+
+app.start = function () {
   // start the web server
-  return app.listen(function() {
+  return app.listen(function () {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
@@ -18,12 +22,41 @@ app.start = function() {
   });
 };
 
+// Create an instance of PassportConfigurator with the app instance
+var PassportConfigurator = require('loopback-component-passport').PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, function (err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
   if (require.main === module)
     app.start();
 });
+
+// Load the provider configurations
+var config = {};
+try {
+  config = require('../providers.json');
+} catch (err) {
+  console.error('Please configure your passport strategy in `providers.json`.');
+  console.error('Copy `providers.json.template` to `providers.json` and replace the clientID/clientSecret values with your own.');
+  process.exit(1);
+}
+// Initialize passport
+passportConfigurator.init();
+
+// Set up related models
+passportConfigurator.setupModels({
+  userModel: app.models.user,
+  userIdentityModel: app.models.userIdentity,
+  userCredentialModel: app.models.userCredential
+});
+// Configure passport strategies for third party auth providers
+for (var s in config) {
+  var c = config[s];
+  c.session = c.session !== false;
+  passportConfigurator.configureProvider(s, c);
+}
