8449323cc6c78526839a060a8390f8355c640a4a
Jian Han <jianhan@mail.com>
2018-09-30 12:11:04 +1000

    [feat] Added initialize script for built in tables

---
+++ server/create-lb-tables.js
@@ -0,0 +1,9 @@
+const server = require('./server');
+const ds = server.dataSources.mongodb;
+const lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+
+ds.automigrate(lbTables, er => {
+  if (er) throw er;
+  console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
