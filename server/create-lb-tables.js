990864f27f7d1bc2bbf542ffbac9552fbab9bb57
Jian Han <jianhan@mail.com>
2018-09-30 12:33:26 +1000

    [feat] Added passport integration

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -1,6 +1,6 @@
 const server = require('./server');
 const ds = server.dataSources.mongodb;
-const lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+const lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'ApplicationCredential', 'UserCredential', 'UserIdentity'];
 
 ds.automigrate(lbTables, er => {
   if (er) throw er;
